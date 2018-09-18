d7873d1ec1551a4662703df4738d5e52e977aa17
Harlan Heath <harlandheath@gmail.com>
2018-09-18 13:58:55 -0500

    components working on front end

--- sim-practice/src/component/Dashboard/Dashboard.js
+++ sim-practice/src/component/Dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Product from "../Product/Product";
 
 export default class Dashboard extends Component {
   render() {
     return (
       <div>
         <p>Dashboard</p>
+        <Product />
       </div>
     );
   }
