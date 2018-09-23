28100daf0194f34d81cba5fa4ac3f8b4be66ed08
Harlan Heath <harlandheath@gmail.com>
2018-09-23 18:33:39 -0500

    adjusting product cards

--- sim-practice/src/component/Dashboard/Dashboard.js
+++ sim-practice/src/component/Dashboard/Dashboard.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import "./Dashboard.css";
 import Product from "../Product/Product";
 
 export default class Dashboard extends Component {
   render() {
     let inventory = this.props.inventory.map((product, index) => {
       return (
-        <div key={index}>
+        <div key={index} className="product-card">
           <p>{product.img}</p>
           <p>{product.price}</p>
           <p>{product.name}</p>
